a1ad11d4004759fdd3407b1845d64b4f81a0e60b
aacase <aacase@gmail.com>
2015-03-20 11:39:53 -0500

    more roles stuff

--- client/startup/default.js
+++ client/startup/default.js
@@ -1,4 +1,6 @@
 Meteor.startup(function () {
 
 	Session.setDefault('currentIncidents', false);
+
+	
 });
--- server/startup/loadUsers.js
+++ server/startup/loadUsers.js
@@ -15,4 +15,15 @@ Meteor.startup(function () {
   for (key in users) if (users.hasOwnProperty(key)) {
     loadUser(users[key]);
   }
+  Roles.addUsersToRoles("CEvvK6fcwG9qSWvNr", ['admin'])
 });
+
+// Accounts.validateNewUser(function (user) {
+//    var loggedInUser = Meteor.user();
+
+//    if (Roles.userIsInRole(loggedInUser, ['admin','manage-users'])) {
+//      return true;
+//    }
+
+//    throw new Meteor.Error(403, "Not authorized to create new users");
+//  });
